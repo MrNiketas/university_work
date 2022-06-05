74d42c342c96a295f38a2c3702f8128c44f817ae
MrNiketas <mr.niketas@gmail.com>
2022-06-05 23:28:28 +0300

    up

---
+++ CourseWork/python/db.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+class Database:
+
+    def __init__(self, db_file):
+        #Инициализация базы данных
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
+
+    def user_exists(self, user_id):
+        #Проверка, есть ли пользователь в БД
+        result = self.cursor.execute("SELECT id FROM 'users' WHERE  'user_id' = ?", (user_id))
+
+    def add_user(self, user_id):
+        # Добавление
+        self.cursor.execute("INSERT INTO 'users' (user_id) VALUE (?)", (user_id,))
--- CourseWork/python/main.py
+++ CourseWork/python/main.py
@@ -1,1 +1,13 @@
-import matplotlib
+import sqlite3
+
+conn = sqlite3.connect('telebotAcq.db')
+cursor = conn.cursor()
+
+cursor.execute("insert into profile values (55, 'nick', 21, 'tmb', 'm', 'link', 'lox')")
+conn.commit()
+
+cursor.execute("select * from profile")
+results=cursor.fetchall()
+print(results)
+
+conn.close()
