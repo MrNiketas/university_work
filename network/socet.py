85ab66006531a0b3c4513d88e97dc7cce6f85c1f
MrNiketas <mr.niketas@gmail.com>
2022-06-13 19:02:47 +0300

    up

---
+++ network/socet.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import socket
+
+sock = socket.socket()
+sock.connect(('localhost', 9090))
+h = 'hello, world!'
+h = bytes(h, encoding = "utf8")
+print(h)
+sock.send(h)
+data = sock.recv(1024)
+sock.close()
+
+print(data)
